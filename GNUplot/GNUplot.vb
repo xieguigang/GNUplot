431ffdf269032edb0d166b9428b5cf48c9138207
xieguigang <xie.guigang@gmail.com>
2016-09-29 14:51:08 +0800

    split source file

--- GNUplot/GNUplot.vb
+++ GNUplot/GNUplot.vb
@@ -1,11 +1,12 @@
-Imports System.Collections.Generic
 Imports System.Diagnostics
 Imports System.IO
-Imports System.Threading
-Imports System.Linq
+Imports Microsoft.VisualBasic
 Imports Microsoft.VisualBasic.Language
 
-Class GnuPlot
+''' <summary>
+''' 
+''' </summary>
+Public Class GnuPlot
     Public Shared PathToGnuplot As String = "C:\Program Files (x86)\gnuplot\bin"
     Private Shared ExtPro As Process
     Private Shared GnupStWr As StreamWriter
@@ -581,120 +582,4 @@ Class GnuPlot
     Public Shared Sub Close()
         ExtPro.CloseMainWindow()
     End Sub
-End Class
-
-Enum PointStyles
-    Dot = 0
-    Plus = 1
-    X = 2
-    Star = 3
-    DotSquare = 4
-    SolidSquare = 5
-    DotCircle = 6
-    SolidCircle = 7
-    DotTriangleUp = 8
-    SolidTriangleUp = 9
-    DotTriangleDown = 10
-    SolidTriangleDown = 11
-    DotDiamond = 12
-    SolidDiamond = 13
-End Enum
-
-Enum PlotTypes
-    PlotFileOrFunction
-    PlotY
-    PlotXY
-    ContourFileOrFunction
-    ContourXYZ
-    ContourZZ
-    ContourZ
-    ColorMapFileOrFunction
-    ColorMapXYZ
-    ColorMapZZ
-    ColorMapZ
-    SplotFileOrFunction
-    SplotXYZ
-    SplotZZ
-    SplotZ
-End Enum
-
-Class StoredPlot
-    Public File As String = Nothing
-    Public [Function] As String = Nothing
-    Public X As Double()
-    Public Y As Double()
-    Public Z As Double()
-    Public ZZ As Double(,)
-    Public YSize As Integer
-    Public Options As String
-    Public PlotType As PlotTypes
-    Public LabelContours As Boolean
-
-    Public Sub New()
-    End Sub
-    Public Sub New(functionOrfilename As String, Optional options__1 As String = "", Optional plotType__2 As PlotTypes = PlotTypes.PlotFileOrFunction)
-        If IsFile(functionOrfilename) Then
-            File = functionOrfilename
-        Else
-            [Function] = functionOrfilename
-        End If
-        Options = options__1
-        PlotType = plotType__2
-    End Sub
-
-    Public Sub New(y__1 As Double(), Optional options__2 As String = "")
-        Y = y__1
-        Options = options__2
-        PlotType = PlotTypes.PlotY
-    End Sub
-
-    Public Sub New(x__1 As Double(), y__2 As Double(), Optional options__3 As String = "")
-        X = x__1
-        Y = y__2
-        Options = options__3
-        PlotType = PlotTypes.PlotXY
-    End Sub
-
-    '3D data
-    Public Sub New(sizeY As Integer, z__1 As Double(), Optional options__2 As String = "", Optional plotType__3 As PlotTypes = PlotTypes.SplotZ)
-        YSize = sizeY
-        Z = z__1
-        Options = options__2
-        PlotType = plotType__3
-    End Sub
-
-    Public Sub New(x__1 As Double(), y__2 As Double(), z__3 As Double(), Optional options__4 As String = "", Optional plotType__5 As PlotTypes = PlotTypes.SplotXYZ)
-        If x__1.Length < 2 Then
-            YSize = 1
-        Else
-            For YSize = 1 To x__1.Length - 1
-                If x__1(YSize) <> x__1(YSize - 1) Then
-                    Exit For
-                End If
-            Next
-        End If
-        Z = z__3
-        Y = y__2
-        X = x__1
-        Options = options__4
-        PlotType = plotType__5
-    End Sub
-
-    Public Sub New(zz__1 As Double(,), Optional options__2 As String = "", Optional plotType__3 As PlotTypes = PlotTypes.SplotZZ)
-        ZZ = zz__1
-        Options = options__2
-        PlotType = plotType__3
-    End Sub
-
-    Private Function IsFile(functionOrFilename As String) As Boolean
-        Dim dot As Integer = functionOrFilename.LastIndexOf(".")
-        If dot < 1 Then
-            Return False
-        End If
-        If Char.IsLetter(functionOrFilename(dot - 1)) OrElse Char.IsLetter(functionOrFilename(dot + 1)) Then
-            Return True
-        End If
-        Return False
-    End Function
-
 End Class
--- GNUplot/Properties/AssemblyInfo.vb
+++ GNUplot/Properties/AssemblyInfo.vb
@@ -5,13 +5,13 @@ Imports System.Runtime.InteropServices
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-<Assembly: AssemblyTitle("GnuplotCSharp")>
-<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyTitle("GNUplot.NET")>
+<Assembly: AssemblyDescription("Gnuplot is a portable command-line driven graphing utility for Linux, OS/2, MS Windows, OSX, VMS, and many other platforms. The source code is copyrighted but freely distributed (i.e., you don't have to pay for it). It was originally created to allow scientists and students to visualize mathematical functions and data interactively, but has grown to support many non-interactive uses such as web scripting. It is also used as a plotting engine by third-party applications like Octave. Gnuplot has been supported and under active development since 1986.")>
 <Assembly: AssemblyConfiguration("")>
-<Assembly: AssemblyCompany("Microsoft")>
-<Assembly: AssemblyProduct("GnuplotCSharp")>
-<Assembly: AssemblyCopyright("Copyright © Microsoft 2012")>
-<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCompany("MIT")>
+<Assembly: AssemblyProduct("GNUplot.NET")>
+<Assembly: AssemblyCopyright("Copyright © MIT license 2016")>
+<Assembly: AssemblyTrademark("Microsoft.VisualBasic")>
 <Assembly: AssemblyCulture("")>
 
 ' Setting ComVisible to false makes the types in this assembly not visible 
