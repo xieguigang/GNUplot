89e670a551f3bcad7f4ffda759e183ef8ca8969b
guigang xie <xie.guigang@gcmodeller.org>
2023-11-02 09:23:53 +0800

    run package msbuild

---
+++ @export/gnuplot.d.ts
@@ -0,0 +1,8 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "gnuplot" from "GNUplot";
+//
+// ref=GNUplot.Rscript@GNUplot, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
