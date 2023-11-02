a619f1f332668577d40f8ce06d85dc1a09242fd2
guigang xie <xie.guigang@gcmodeller.org>
2023-11-02 10:57:10 +0800

    get plot data

--- src/GNUplot.Interop/Internal/Script/ScriptGenerator.vb
+++ src/GNUplot.Interop/Internal/Script/ScriptGenerator.vb
@@ -5,10 +5,21 @@ Imports System.Text
 Public Class ScriptGenerator
 
     ReadOnly plotstring As New StringBuilder
+    ReadOnly buffer As New StringBuilder
+    ReadOnly text As TextWriter = New StringWriter(buffer)
 
     Friend Shared Function plotPath(path As String) As String
         Return """" & path.Replace("\", "\\") & """"
     End Function
+
+    Public Overrides Function ToString() As String
+        Return plotstring.ToString
+    End Function
+
+    Public Sub GetScript(<Out> ByRef plotstring As String, <Out> ByRef gnuplot As String)
+        plotstring = Me.plotstring.ToString
+        gnuplot = Me.buffer.ToString
+    End Sub
 
     Public Sub Push(p As StoredPlot, <Out> ByRef plot__1 As String, i As Integer)
         Dim defcntopts = If((p.Options.Length > 0 AndAlso (p.Options.Contains(" w") OrElse p.Options(0) = "w"c)), " ", " with lines ")
@@ -27,33 +38,33 @@ Public Class ScriptGenerator
 
             Case PlotTypes.ContourFileOrFunction
                 contfile = Path.GetTempPath() & "_cntrtempdata" & i & ".dat"
-                makeContourFile((If(p.File IsNot Nothing, plotPath(p.File), p.[Function])), contfile)
+                makeContourFile((If(p.File IsNot Nothing, plotPath(p.File), p.[Function])), contfile, gnuplot:=text)
                 If p.LabelContours Then
-                    setContourLabels(contfile)
+                    setContourLabels(contfile, gnuplot:=text)
                 End If
                 plotstring.Append(plot__1 & plotPath(contfile) & defcntopts & p.Options)
 
             Case PlotTypes.ContourXYZ
                 contfile = Path.GetTempPath() & "_cntrtempdata" & i & ".dat"
-                makeContourFile(p.X, p.Y, p.Z, contfile)
+                makeContourFile(p.X, p.Y, p.Z, contfile, gnuplot:=text)
                 If p.LabelContours Then
-                    setContourLabels(contfile)
+                    setContourLabels(contfile, gnuplot:=text)
                 End If
                 plotstring.Append(plot__1 & plotPath(contfile) & defcntopts & p.Options)
 
             Case PlotTypes.ContourZZ
                 contfile = Path.GetTempPath() & "_cntrtempdata" & i & ".dat"
-                makeContourFile(p.ZZ, contfile)
+                makeContourFile(p.ZZ, contfile, gnuplot:=text)
                 If p.LabelContours Then
-                    setContourLabels(contfile)
+                    setContourLabels(contfile, gnuplot:=text)
                 End If
                 plotstring.Append(plot__1 & plotPath(contfile) & defcntopts & p.Options)
 
             Case PlotTypes.ContourZ
                 contfile = Path.GetTempPath() & "_cntrtempdata" & i & ".dat"
-                makeContourFile(p.YSize, p.Z, contfile)
+                makeContourFile(p.YSize, p.Z, contfile, gnuplot:=text)
                 If p.LabelContours Then
-                    setContourLabels(contfile)
+                    setContourLabels(contfile, gnuplot:=text)
                 End If
                 plotstring.Append(plot__1 & plotPath(contfile) & defcntopts & p.Options)
 
@@ -71,6 +82,8 @@ Public Class ScriptGenerator
                 plotstring.Append(plot__1 & """-"" " & "matrix with image " & p.Options)
 
         End Select
+
+        Call text.Flush()
     End Sub
 
 End Class
