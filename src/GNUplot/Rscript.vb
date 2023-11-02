25eaf572d153d7a9ee0b93736022af6e2ce2b92e
guigang xie <xie.guigang@gcmodeller.org>
2023-11-02 09:56:26 +0800

    config run test for scatter

--- src/GNUplot/Rscript.vb
+++ src/GNUplot/Rscript.vb
@@ -1,5 +1,8 @@
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Interop
+Imports SMRUCC.Rsharp.Runtime.Vectorization
 
 <Package("gnuplot")>
 Public Class Rscript
@@ -13,5 +16,21 @@ Public Class Rscript
     Public Function config(gnuplot As String) As Boolean
         GNUplotEnvironment.Config = gnuplot
         Return True
+    End Function
+
+    ''' <summary>
+    ''' Create x,y scatter plot
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <param name="y"></param>
+    ''' <param name="env"></param>
+    ''' <returns></returns>
+    <ExportAPI("scatter")>
+    Public Function scatter(<RRawVectorArgument> x As Object, <RRawVectorArgument> y As Object, Optional env As Environment = Nothing) As Object
+        Dim vx As Double() = CLRVector.asNumeric(x)
+        Dim vy As Double() = CLRVector.asNumeric(y)
+
+        Call GNUplot.Plot(vx, vy)
+
     End Function
 End Class
