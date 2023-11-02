9926a63c3f2e9ddec69e36b4c1ca1a5ada960f64
guigang xie <xie.guigang@gcmodeller.org>
2023-11-02 09:37:37 +0800

    add functions for run configs

--- src/GNUplot/Rscript.vb
+++ src/GNUplot/Rscript.vb
@@ -1,7 +1,17 @@
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
 <Package("gnuplot")>
 Public Class Rscript
 
-
+    ''' <summary>
+    ''' config of the gnuplot executable file path
+    ''' </summary>
+    ''' <param name="gnuplot">the user configed custom gnuplot path location.</param>
+    ''' <returns></returns>
+    <ExportAPI("config")>
+    Public Function config(gnuplot As String) As Boolean
+        GNUplotEnvironment.Config = gnuplot
+        Return True
+    End Function
 End Class
